d6dea7e2c2c83ec28fc161972d1c8d6e46ae9a40
James de Winton <james.dewinton@gmail.com>
2019-04-02 11:20:50 +0100

    users created

--- api.py
+++ api.py
@@ -4,4 +4,23 @@ from flask_restful import Resource, Api, reqparse
 app = Flask(__name__)
 api = Api(app)
 
-users 
+users = [
+    {
+    "name": "James",
+    "age": 20,
+    "occupation": "software engineer"
+
+    },
+    {
+    "name": "Max",
+    "age": 20,
+    "occupation": "retail assistant"
+
+    },
+    {
+    "name": "Felix",
+    "age": 20,
+    "occupation": "Model"
+    }
+
+]
