6d1079907274d589dd0bad9be370d14d80f72069
James de Winton <james.dewinton@gmail.com>
2019-04-02 14:40:20 +0100

    user function delete, added to user class

--- app.py
+++ app.py
@@ -52,6 +52,26 @@ class User(Resource):
         return user, 201
 
     def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("age")
+        parser.add_argument("occupation")
+        args = parser.parse_args()
+
+        for user in users:
+            if(name == user["name"]):
+                user["age"] = args["age"]
+                user["occupation"] = args["occupation"]
+                return user, 200
+        user = {
+            "name": name,
+            "age": args["age"],
+            "occupation": args["occupation"]
+        }
+        users.append(user)
+        return user, 201
 
 
     def delete(self, name):
+        global users
+        users = [user for user in users if user["name"] != name]
+        return "{ is deleted}.".format(name, 200)
